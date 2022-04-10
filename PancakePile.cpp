a8077ee81246aacf2eeffda9c518af1e73fd2f22
Diego Angulo <angulo.ramires.d@phystech.edu>
2022-04-11 01:03:41 +0300

    2-Approximation algorithm added

--- PancakePile.cpp
+++ PancakePile.cpp
@@ -4,55 +4,180 @@
 
 #include "PancakePile.h"
 
-PancakePile::PancakePile(size_t size) : num_pancakes_(size){
+PancakePile::PancakePile(int size) : num_pancakes_(size) {
     //We initialize the size of each pancake, from 1 to size
-    order_.resize(size);
-    for(size_t i = 0; i < order_.size(); ++i){
-        order_[i] = i + 1;
+    order_.resize(size + 2);
+    positions_.resize(size + 2);
+    order_[0] = 0;
+    order_[size + 1] = size + 1;
+    for (size_t i = 1; i <= num_pancakes_; ++i) {
+        order_[i] = i;
     }
 
     //Random permutation of pancakes
     std::random_device rd;
     std::mt19937 g(rd());
-    std::shuffle(order_.begin(), order_.end(), g);
+    std::shuffle(order_.begin() + 1, order_.end() - 1, g);
+
+    for (size_t i = 1; i <= num_pancakes_; ++i) {
+        positions_[order_[i]] = i;
+    }
+    positions_[0] = 0;
+    positions_[num_pancakes_ + 1] = num_pancakes_ + 1;
+}
+
+PancakePile::PancakePile(const std::vector<int> &permutation) {
+    num_pancakes_ = int(permutation.size());
+    order_.resize(permutation.size() + 2);
+    order_[0] = 0;
+    order_[num_pancakes_ + 1] = int(num_pancakes_) + 1;
+    for (size_t i = 0; i < permutation.size(); ++i) {
+        order_[i + 1] = permutation[i];
+    }
+
+    positions_.resize(num_pancakes_ + 2);
+    for (size_t i = 1; i <= num_pancakes_; ++i) {
+        positions_[order_[i]] = i;
+    }
+    positions_[0] = 0;
+    positions_[num_pancakes_ + 1] = num_pancakes_ + 1;
+}
+
+void PancakePile::Print(int pos_spatula) {
+    //Prints the stack of pancakes and the position in which the spatula is in
+    for (int i = 0; i < order_.size(); ++i) {
+        std::cout << order_[i] << " ";
+        if (i == pos_spatula)
+            std::cout << "| ";
+    }
+    std::cout << "\n";
 }
 
 void PancakePile::Print() {
-    for(auto i : order_)
-        std::cout << i << ' ';
-    std::cout << '\n';
+    Print(order_.size() + 1); //So it never prints a spatula
 }
 
-size_t PancakePile::SortInitial() {
-    size_t pos_pancake, steps = 0;
+size_t PancakePile::Sort_FirstAlgo() {
+    int pos_pancake;
+    steps = 0;
 
-    for(size_t i = 1; i <= num_pancakes_; ++i){
-        Print();
+    for (int i = num_pancakes_; i >= 1; --i) {
         pos_pancake = SearchPancake(i);
-        if(pos_pancake == i - 1) continue;
+        if (pos_pancake == i) continue;
 
-        if(pos_pancake != num_pancakes_ - 1) {
+        if (pos_pancake > 1) {
             Flip(pos_pancake);
             steps++;
         }
 
-        Flip(i - 1);
+        Flip(i);
         steps++;
     }
 
     return steps;
 }
 
-void PancakePile::Flip(size_t init) {
-    std::reverse(order_.begin() + init, order_.end());
+void PancakePile::Flip(int elements) {
+    if(elements <= 1)
+        return;
+    steps++;
+    Print(elements);
+    for (int i = 1; i <= elements / 2; ++i) {
+        std::swap(order_[i], order_[elements - i + 1]);
+        positions_[order_[i]] = i;
+        positions_[order_[elements - i + 1]] = elements - i + 1;
+    }
 }
 
-size_t PancakePile::SearchPancake(size_t pancake) {
-    for(size_t i =0; i < num_pancakes_; ++i)
-        if(order_[i] == pancake) return i;
+int PancakePile::SearchPancake(int pancake) {
+    for (int i = 1; i <= num_pancakes_; ++i)
+        if (order_[i] == pancake) return i;
 }
 
-PancakePile::PancakePile(const std::vector<size_t>& permutation) {
-    num_pancakes_ = permutation.size();
-    order_ = permutation;
+size_t PancakePile::Sort_SecondAlgo() {
+    bool re_start = true;
+    steps = 0;
+
+    while (re_start) {
+        re_start = false;
+        //Check blue arc of type 1 with i = 1
+        if (CheckType1(order_[1], order_[1] - 1)) {
+            Flip(positions_[order_[1] - 1] - 1);
+            re_start = true;
+        } else if (CheckType1(order_[1], order_[1] + 1)) {
+            Flip(positions_[order_[1] + 1] - 1);
+            re_start = true;
+        }
+        if(re_start)continue;
+        for (int i = 1; i < num_pancakes_; ++i) {
+            if(CheckType2(i, i + 1)){
+                int pos1, pos2;
+                pos1 = std::min(positions_[i], positions_[i + 1]);
+                pos2 = std::max(positions_[i], positions_[i + 1]);
+                Flip(pos2);
+                Flip(pos2 - pos1);
+                re_start = true;
+                break;
+            }
+        }
+        if(re_start)continue;
+        for (int i = 0; i <= num_pancakes_; ++i) {
+            if(CheckType3(i, i + 1)){
+                int pos1, pos2;
+                pos1 = std::min(positions_[i], positions_[i + 1]);
+                pos2 = std::max(positions_[i], positions_[i + 1]);
+                Flip(pos1);
+                Flip(pos2 - 1);
+                re_start = true;
+                break;
+            }
+        }
+    }
+
+    //Final step, Lemma 6
+    std::vector<int> lengths;
+    int p_actual, p_previous = 0;
+    for(size_t i = 1; i <= num_pancakes_;){
+        p_actual = order_[i];
+        lengths.push_back(p_actual - p_previous);
+        p_previous = p_actual;
+        i = p_actual + 1;
+    }
+
+    for(size_t i = 0; i < lengths.size();++i){
+        Flip(num_pancakes_);
+        Flip(num_pancakes_ - lengths[i]);
+    }
+
+    return steps;
 }
+
+bool PancakePile::CheckType1(int val1, int val2) {
+    if(positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Left(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
+}
+
+bool PancakePile::CheckType2(int val1, int val2) {
+    if(positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Right(val1) - val1) != 1 && abs(Right(val2) - val2) != 1);
+}
+
+bool PancakePile::CheckType3(int val1, int val2) {
+    if(positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Right(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
+}
+
+int PancakePile::Left(int val) {
+    return order_[positions_[val] - 1];
+}
+
+int PancakePile::Right(int val) {
+    return order_[positions_[val] + 1];
+}
+
+
+
+
--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 
 int main() {
     PancakePile pancakes(4);
-    size_t steps = pancakes.SortInitial();
+    size_t steps = pancakes.Sort_FirstAlgo();
     std::cout<<'\n';
     pancakes.Print();
+    std::cout << steps << '\n';
+
+    std::vector<int> aux(9);
+    aux = {8, 2, 1, 9, 5, 6, 3, 4, 7};
+    PancakePile pancakes_2(aux);
+    steps = pancakes_2.Sort_SecondAlgo();
+    std::cout<<'\n';
+    pancakes_2.Print();
+    std::cout << steps << '\n';
+
+    PancakePile pancakes_3(10);
+    steps = pancakes_3.Sort_SecondAlgo();
+    std::cout<<'\n';
+    pancakes_3.Print();
     std::cout << steps << '\n';
     return 0;
 }
