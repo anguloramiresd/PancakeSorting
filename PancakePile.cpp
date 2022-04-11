11c9b18e61b1b1b87e6bc754d985dd8390477251
Diego Angulo <angulo.ramires.d@phystech.edu>
2022-04-11 14:21:43 +0300

    Brute Force solution added

--- PancakePile.cpp
+++ PancakePile.cpp
@@ -57,6 +57,29 @@ void PancakePile::Print() {
     Print(order_.size() + 1); //So it never prints a spatula
 }
 
+void PancakePile::Flip(std::vector<int> &aux, int elements) {
+    for (int i = 1; i <= elements / 2; ++i) {
+        std::swap(aux[i], aux[elements - i + 1]);
+    }
+}
+
+void PancakePile::Flip(int elements) {
+    if (elements <= 1)
+        return;
+    steps++;
+    Print(elements);
+    for (int i = 1; i <= elements / 2; ++i) {
+        std::swap(order_[i], order_[elements - i + 1]);
+        positions_[order_[i]] = i;
+        positions_[order_[elements - i + 1]] = elements - i + 1;
+    }
+}
+
+int PancakePile::SearchPancake(int pancake) {
+    for (int i = 1; i <= num_pancakes_; ++i)
+        if (order_[i] == pancake) return i;
+}
+
 size_t PancakePile::Sort_FirstAlgo() {
     int pos_pancake;
     steps = 0;
@@ -77,23 +100,6 @@ size_t PancakePile::Sort_FirstAlgo() {
     return steps;
 }
 
-void PancakePile::Flip(int elements) {
-    if(elements <= 1)
-        return;
-    steps++;
-    Print(elements);
-    for (int i = 1; i <= elements / 2; ++i) {
-        std::swap(order_[i], order_[elements - i + 1]);
-        positions_[order_[i]] = i;
-        positions_[order_[elements - i + 1]] = elements - i + 1;
-    }
-}
-
-int PancakePile::SearchPancake(int pancake) {
-    for (int i = 1; i <= num_pancakes_; ++i)
-        if (order_[i] == pancake) return i;
-}
-
 size_t PancakePile::Sort_SecondAlgo() {
     bool re_start = true;
     steps = 0;
@@ -108,9 +114,9 @@ size_t PancakePile::Sort_SecondAlgo() {
             Flip(positions_[order_[1] + 1] - 1);
             re_start = true;
         }
-        if(re_start)continue;
+        if (re_start)continue;
         for (int i = 1; i < num_pancakes_; ++i) {
-            if(CheckType2(i, i + 1)){
+            if (CheckType2(i, i + 1)) {
                 int pos1, pos2;
                 pos1 = std::min(positions_[i], positions_[i + 1]);
                 pos2 = std::max(positions_[i], positions_[i + 1]);
@@ -120,9 +126,9 @@ size_t PancakePile::Sort_SecondAlgo() {
                 break;
             }
         }
-        if(re_start)continue;
+        if (re_start)continue;
         for (int i = 0; i <= num_pancakes_; ++i) {
-            if(CheckType3(i, i + 1)){
+            if (CheckType3(i, i + 1)) {
                 int pos1, pos2;
                 pos1 = std::min(positions_[i], positions_[i + 1]);
                 pos2 = std::max(positions_[i], positions_[i + 1]);
@@ -133,41 +139,25 @@ size_t PancakePile::Sort_SecondAlgo() {
             }
         }
     }
+    std::cout << "Final Sorting\n";
+    Print();
 
     //Final step, Lemma 6
     std::vector<int> lengths;
     int p_actual, p_previous = 0;
-    for(size_t i = 1; i <= num_pancakes_;){
+    for (size_t i = 1; i <= num_pancakes_;) {
         p_actual = order_[i];
         lengths.push_back(p_actual - p_previous);
         p_previous = p_actual;
         i = p_actual + 1;
     }
 
-    for(size_t i = 0; i < lengths.size();++i){
+    for (size_t i = 0; i < lengths.size(); ++i) {
         Flip(num_pancakes_);
         Flip(num_pancakes_ - lengths[i]);
     }
 
     return steps;
-}
-
-bool PancakePile::CheckType1(int val1, int val2) {
-    if(positions_[val1] > positions_[val2])
-        std::swap(val1, val2);
-    return (abs(Left(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
-}
-
-bool PancakePile::CheckType2(int val1, int val2) {
-    if(positions_[val1] > positions_[val2])
-        std::swap(val1, val2);
-    return (abs(Right(val1) - val1) != 1 && abs(Right(val2) - val2) != 1);
-}
-
-bool PancakePile::CheckType3(int val1, int val2) {
-    if(positions_[val1] > positions_[val2])
-        std::swap(val1, val2);
-    return (abs(Right(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
 }
 
 int PancakePile::Left(int val) {
@@ -178,6 +168,43 @@ int PancakePile::Right(int val) {
     return order_[positions_[val] + 1];
 }
 
+bool PancakePile::CheckType1(int val1, int val2) {
+    if (positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Left(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
+}
 
+bool PancakePile::CheckType2(int val1, int val2) {
+    if (positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Right(val1) - val1) != 1 && abs(Right(val2) - val2) != 1);
+}
 
+bool PancakePile::CheckType3(int val1, int val2) {
+    if (positions_[val1] > positions_[val2])
+        std::swap(val1, val2);
+    return (abs(Right(val1) - val1) != 1 && abs(Left(val2) - val2) != 1);
+}
 
+size_t PancakePile::Sort_BruteForce() {
+    //Uses a BFS to check all possible outcomes with minimal number of steps
+    std::map<std::vector<int>, int> distance;
+    std::queue<std::vector<int>> q;
+    std::vector<int> actual, aux;
+    distance[order_] = 0;
+    q.push(order_);
+    while (!q.empty()) {
+        actual = q.front();
+        q.pop();
+        for (int i = 2; i <= num_pancakes_; ++i) {
+            aux = actual;
+            Flip(aux, i);
+            if(distance[aux] == 0 && aux != order_){
+                q.push(aux);
+                distance[aux] = distance[actual] + 1;
+            }
+        }
+    }
+    std::sort(order_.begin(), order_.end());
+    return distance[order_];
+}
